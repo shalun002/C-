e680ac924365d1310997a25a71dc03cc875052d8
БлиновС <@223_15>
2018-02-12 19:09:14 +0600

    first

---
+++ Project/SEP-171.Module_/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEP_171.Module_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
